0ba0f088e9784c82795e9f27235b4e234d16a6cb
Sygikal <xtin8873@gmail.com>
2025-09-17 16:05:33 -0700

    more robust tooltip handling

--- src/main/java/net/skillz/util/TooltipUtil.java
+++ src/main/java/net/skillz/util/TooltipUtil.java
@@ -75,59 +75,19 @@ public class TooltipUtil {
         if (client.player != null) {
             LevelManager levelManager = ((LevelManagerAccess) client.player).getLevelManager();
             boolean showLines = client.player.isCreative() || !ConfigInit.CLIENT.hideReachedLevels || client.currentScreen instanceof SkillRestrictionScreen; // Add all lines, not only the missing ones
-            Formatting format = Formatting.GRAY;
 
             if (stack.getItem() instanceof BlockItem blockItem) {
                 int blockId = Registries.BLOCK.getRawId(blockItem.getBlock());
-                if (showLines || !levelManager.hasRequiredBlockLevel(blockItem.getBlock())) {
-                    if (LevelManager.BLOCK_RESTRICTIONS.containsKey(blockId)) {
-                        PlayerRestriction playerRestriction = LevelManager.BLOCK_RESTRICTIONS.get(blockId);
-                        lines.add(USABLE);
-                        for (Map.Entry<String, Integer> entry : playerRestriction.getSkillLevelRestrictions().entrySet()) {
-                            boolean noHasLevel = levelManager.getSkillLevel(entry.getKey()) < entry.getValue();
-                            if (showLines || noHasLevel) {
-                                lines.add(getRestrictionKey(entry.getKey(), entry.getValue()).formatted(noHasLevel ? Formatting.RED : Formatting.GREEN));
-                            }
-                        }
-                    }
-                }
-                if (showLines || !levelManager.hasRequiredMiningLevel(blockItem.getBlock())) {
-                    if (LevelManager.MINING_RESTRICTIONS.containsKey(blockId)) {
-                        PlayerRestriction playerRestriction = LevelManager.MINING_RESTRICTIONS.get(blockId);
-                        lines.add(MINEABLE);
-                        for (Map.Entry<String, Integer> entry : playerRestriction.getSkillLevelRestrictions().entrySet()) {
-                            boolean noHasLevel = levelManager.getSkillLevel(entry.getKey()) < entry.getValue();
-                            if (showLines || noHasLevel) {
-                                lines.add(getRestrictionKey(entry.getKey(), entry.getValue()).formatted(noHasLevel ? Formatting.RED : Formatting.GREEN));
-                            }
-                        }
-                    }
-                }
+                addLines(lines, USABLE, blockId, levelManager, showLines, LevelManager.BLOCK_RESTRICTIONS);
+                addLines(lines, MINEABLE, blockId, levelManager, showLines, LevelManager.MINING_RESTRICTIONS);
+            }else if (stack.getItem() instanceof SpawnEggItem spawnEggItem) {
+                addLines(lines, USABLE, Registries.ENTITY_TYPE.getRawId(spawnEggItem.getEntityType(stack.getNbt())), levelManager, showLines, LevelManager.ENTITY_RESTRICTIONS);
             }
+
             int itemId = Registries.ITEM.getRawId(stack.getItem());
-            if (showLines || !levelManager.hasRequiredItemLevel(stack.getItem())) {
-                if (LevelManager.ITEM_RESTRICTIONS.containsKey(itemId)) {
-                    lines.add(USABLE);
-                    for (Map.Entry<String, Integer> entry : levelManager.getRequiredItemLevel(stack.getItem()).entrySet()) {
-                        boolean noHasLevel = levelManager.getSkillLevel(entry.getKey()) < entry.getValue();
-                        if (showLines || noHasLevel) {
-                            lines.add(getRestrictionKey(entry.getKey(), entry.getValue()).formatted(noHasLevel ? Formatting.RED : Formatting.GREEN));
-                        }
-                    }
-                }
-            }
-            if (showLines || !levelManager.hasRequiredCraftingLevel(stack.getItem())) {
-                if (LevelManager.CRAFTING_RESTRICTIONS.containsKey(itemId)) {
-                    PlayerRestriction playerRestriction = LevelManager.CRAFTING_RESTRICTIONS.get(itemId);
-                    lines.add(CRAFTABLE);
-                    for (Map.Entry<String, Integer> entry : playerRestriction.getSkillLevelRestrictions().entrySet()) {
-                        boolean noHasLevel = levelManager.getSkillLevel(entry.getKey()) < entry.getValue();
-                        if (showLines || noHasLevel) {
-                            lines.add(getRestrictionKey(entry.getKey(), entry.getValue()).formatted(noHasLevel ? Formatting.RED : Formatting.GREEN));
-                        }
-                    }
-                }
-            }
+            addLines(lines, USABLE, itemId, levelManager, showLines, LevelManager.ITEM_RESTRICTIONS);
+            addLines(lines, CRAFTABLE, itemId, levelManager, showLines, LevelManager.CRAFTING_RESTRICTIONS);
+
             final Map<Enchantment, Integer> enchantments = EnchantmentHelper.get(stack);
 
             if (!enchantments.isEmpty()) {
@@ -151,70 +111,32 @@ public class TooltipUtil {
                     }
                 }
             }
-            if (stack.getItem() instanceof SpawnEggItem spawnEggItem) {
-                if (showLines || !levelManager.hasRequiredEntityLevel(spawnEggItem.getEntityType(stack.getNbt()))) {
-                    int entityId = Registries.ENTITY_TYPE.getRawId(spawnEggItem.getEntityType(stack.getNbt()));
-                    if (LevelManager.ENTITY_RESTRICTIONS.containsKey(entityId)) {
-                        PlayerRestriction playerRestriction = LevelManager.ENTITY_RESTRICTIONS.get(entityId);
-                        lines.add(USABLE);
-                        for (Map.Entry<String, Integer> entry : playerRestriction.getSkillLevelRestrictions().entrySet()) {
-                            if (showLines || levelManager.getSkillLevel(entry.getKey()) < entry.getValue()) {
-                                lines.add(getRestrictionKey(entry.getKey(), entry.getValue()).formatted(Formatting.RED));
-                            }
-                        }
-                    }
-                }
-            }
         }
     }
 
-    // Recommend to play with https://www.curseforge.com/minecraft/mc-mods/health-overlay-fabric
     public static void renderTooltip(MinecraftClient client, DrawContext context) {
-        if (client.crosshairTarget != null && ConfigInit.CLIENT.showLockedBlockInfo) {
+        if (client.crosshairTarget != null && ConfigInit.CLIENT.showRestrictionOnHUD) {
+            boolean showLines = client.player.isCreative() || !ConfigInit.CLIENT.hideReachedLevels;
 
             HitResult hitResult = client.crosshairTarget;
+            LevelManager levelManager = ((LevelManagerAccess) client.player).getLevelManager();
+            List<Text> textList = new ArrayList<>();
+            Identifier identifier = null;
             if (hitResult.getType() == HitResult.Type.ENTITY) {
-                LevelManager levelManager = ((LevelManagerAccess) client.player).getLevelManager();
                 EntityType<?> entityType = ((EntityHitResult) hitResult).getEntity().getType();
-                if (!levelManager.hasRequiredEntityLevel(entityType)) {
-                    List<Text> textList = new ArrayList<>();
-                    textList.add(Text.of(entityType.getName().getString()));
-                    for (Map.Entry<String, Integer> entry : levelManager.getRequiredEntityLevel(entityType).entrySet()) {
-                        Formatting formatting =
-                                levelManager.getSkillLevel(entry.getKey()) < entry.getValue() ? Formatting.RED : Formatting.GREEN;
-                        textList.add(Text.translatable("restriction.skillz." + LevelManager.SKILLS.get(entry.getKey()).id() + ".tooltip", entry.getValue()).formatted(formatting));
-                    }
-                    renderTooltip(client, context, textList,
-                            null, context.getScaledWindowWidth() / 2 + ConfigInit.CLIENT.lockedBlockInfoPosX, ConfigInit.CLIENT.lockedBlockInfoPosY);
-                }
+                textList.add(Text.of(entityType.getName().getString()));
+                addLines(textList, USABLE, Registries.ENTITY_TYPE.getRawId(entityType), levelManager, showLines, LevelManager.ENTITY_RESTRICTIONS);
+
             } else if (hitResult.getType() == HitResult.Type.BLOCK) {
                 Block block = client.world.getBlockState(((BlockHitResult) hitResult).getBlockPos()).getBlock();
-                LevelManager levelManager = ((LevelManagerAccess) client.player).getLevelManager();
-                List<Text> textList = new ArrayList<>();
-                if (!levelManager.hasRequiredMiningLevel(block)) {
-                    textList.add(Text.of(block.getName().getString()));
-                    // textList.add(Text.translatable("item.skillz.mineable.tooltip"));
-                    for (Map.Entry<String, Integer> entry : levelManager.getRequiredMiningLevel(block).entrySet()) {
-                        Formatting formatting =
-                                levelManager.getSkillLevel(entry.getKey()) < entry.getValue() ? Formatting.RED : Formatting.GREEN;
-                        textList.add(Text.translatable("restriction.skillz." + LevelManager.SKILLS.get(entry.getKey()).id() + ".tooltip", entry.getValue()).formatted(formatting));
-                    }
-                }
-                if (!levelManager.hasRequiredBlockLevel(block)) {
-                    if (textList.isEmpty()) {
-                        textList.add(Text.of(block.getName().getString()));
-                    }
-                    textList.add(Text.translatable("restriction.skillz.block_usage"));
-                    for (Map.Entry<String, Integer> entry : levelManager.getRequiredBlockLevel(block).entrySet()) {
-                        Formatting formatting =
-                                levelManager.getSkillLevel(entry.getKey()) < entry.getValue() ? Formatting.RED : Formatting.GREEN;
-                        textList.add(Text.translatable("restriction.skillz." + LevelManager.SKILLS.get(entry.getKey()).id() + ".tooltip", entry.getValue()).formatted(formatting));
-                    }
-                }
-                if (!textList.isEmpty()) {
-                    renderTooltip(client, context, textList,
-                            Registries.BLOCK.getId(block), context.getScaledWindowWidth() / 2 + ConfigInit.CLIENT.lockedBlockInfoPosX, ConfigInit.CLIENT.lockedBlockInfoPosY);
-                }
+                textList.add(Text.of(block.getName().getString()));
+                addLines(textList, MINEABLE, Registries.BLOCK.getRawId(block), levelManager, showLines, LevelManager.MINING_RESTRICTIONS);
+                addLines(textList, USABLE, Registries.BLOCK.getRawId(block), levelManager, showLines, LevelManager.BLOCK_RESTRICTIONS);
+                identifier = Registries.BLOCK.getId(block);
+            }
+            if (textList.size() > 1) {
+                renderTooltip(client, context, textList,
+                        identifier, context.getScaledWindowWidth() / 2 + ConfigInit.CLIENT.hudInfoX, ConfigInit.CLIENT.hudInfoY);
             }
         }
     }
@@ -237,7 +159,7 @@ public class TooltipUtil {
         int colorTwo = 0xBF7F0200; // light border
         int colorThree = 0xBF380000; // darker border
 
-        render(context, x - maxTextWidth / 2 - 3, y + 4, maxTextWidth, textList.size() * 10 + 11, 400, colorStart, colorTwo, colorThree);
+        DrawUtil.render(context, x - maxTextWidth / 2 - 3, y + 4, maxTextWidth, textList.size() * 10 + 11, 400, colorStart, colorTwo, colorThree);
 
         context.getMatrices().translate(0.0, 0.0, 400.0);
 
@@ -255,73 +177,6 @@ public class TooltipUtil {
             context.drawItem(Registries.ITEM.get(identifier).getDefaultStack(), x - maxTextWidth / 2, y + 5);
         }
         context.getMatrices().pop();
-    }
-
-    public static void render(DrawContext context, int x, int y, int width, int height, int z, int background, int borderColorStart, int borderColorEnd) {
-        int i = x - 3;
-        int j = y - 3;
-        int k = width + 3 + 3;
-        int l = height + 3 + 3;
-
-        renderHorizontalLine(context, i, j - 1, k, z, background);
-        renderHorizontalLine(context, i, j + l, k, z, background);
-        renderRectangle(context, i, j, k, l, z, background);
-        renderVerticalLine(context, i - 1, j, l, z, background);
-        renderVerticalLine(context, i + k, j, l, z, background);
-        renderBorder(context, i, j + 1, k, l, z, borderColorStart, borderColorEnd);
-
-        width -= 6;
-        renderHorizontalLine(context, z, x + 3, y + 19, x + 3 + width / 2, y + 20, 0x007F0200, 0xBF7F0200);
-        renderHorizontalLine(context, z, x + 3 + width / 2, y + 19, x + 3 + width, y + 20, 0xBF7F0200, 0x007F0200);
-    }
-
-    private static void renderBorder(DrawContext context, int x, int y, int width, int height, int z, int startColor, int endColor) {
-        renderVerticalLine(context, x, y, height - 2, z, startColor, endColor);
-        renderVerticalLine(context, x + width - 1, y, height - 2, z, startColor, endColor);
-        renderHorizontalLine(context, x, y - 1, width, z, startColor);
-        renderHorizontalLine(context, x, y - 1 + height - 1, width, z, endColor);
-    }
-
-    private static void renderVerticalLine(DrawContext context, int x, int y, int height, int z, int color) {
-        context.fill(x, y, x + 1, y + height, z, color);
-    }
-
-    private static void renderVerticalLine(DrawContext context, int x, int y, int height, int z, int startColor, int endColor) {
-        context.fillGradient(x, y, x + 1, y + height, z, startColor, endColor);
-    }
-
-    private static void renderHorizontalLine(DrawContext context, int x, int y, int width, int z, int color) {
-        context.fill(x, y, x + width, y + 1, z, color);
-    }
-
-    private static void renderRectangle(DrawContext context, int x, int y, int width, int height, int z, int color) {
-        context.fill(x, y, x + width, y + height, z, color);
-    }
-
-    public static void renderHorizontalLine(DrawContext context, int zLevel, int left, int top, int right, int bottom, int startColor, int endColor) {
-        float endAlpha = (float) (endColor >> 24 & 255) / 255.0F;
-        float endRed = (float) (endColor >> 16 & 255) / 255.0F;
-        float endGreen = (float) (endColor >> 8 & 255) / 255.0F;
-        float endBlue = (float) (endColor & 255) / 255.0F;
-        float startAlpha = (float) (startColor >> 24 & 255) / 255.0F;
-        float startRed = (float) (startColor >> 16 & 255) / 255.0F;
-        float startGreen = (float) (startColor >> 8 & 255) / 255.0F;
-        float startBlue = (float) (startColor & 255) / 255.0F;
-
-        RenderSystem.enableDepthTest();
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
-
-        VertexConsumer vertexConsumer = context.getVertexConsumers().getBuffer(RenderLayer.getGui());
-        Matrix4f matrix4f = context.getMatrices().peek().getPositionMatrix();
-
-        vertexConsumer.vertex(matrix4f, right, top, zLevel).color(endRed, endGreen, endBlue, endAlpha);
-        vertexConsumer.vertex(matrix4f, left, top, zLevel).color(startRed, startGreen, startBlue, startAlpha);
-        vertexConsumer.vertex(matrix4f, left, bottom, zLevel).color(startRed, startGreen, startBlue, startAlpha);
-        vertexConsumer.vertex(matrix4f, right, bottom, zLevel).color(endRed, endGreen, endBlue, endAlpha);
-        context.draw();
-        RenderSystem.disableBlend();
     }
 
 }
