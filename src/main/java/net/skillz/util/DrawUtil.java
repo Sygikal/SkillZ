ab062806355f28b046aa346dda8a8b60718b3c83
Sygikal <xtin8873@gmail.com>
2025-09-17 16:04:39 -0700

    move functions

--- src/main/java/net/skillz/util/DrawUtil.java
+++ src/main/java/net/skillz/util/DrawUtil.java
@@ -4,6 +4,9 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
@@ -63,6 +66,73 @@ public class DrawUtil {
         entityRenderDispatcher.setRenderShadows(true);
         context.getMatrices().pop();
         DiffuseLighting.enableGuiDepthLighting();
+    }
+
+    public static void render(DrawContext context, int x, int y, int width, int height, int z, int background, int borderColorStart, int borderColorEnd) {
+        int i = x - 3;
+        int j = y - 3;
+        int k = width + 3 + 3;
+        int l = height + 3 + 3;
+
+        renderHorizontalLine(context, i, j - 1, k, z, background);
+        renderHorizontalLine(context, i, j + l, k, z, background);
+        renderRectangle(context, i, j, k, l, z, background);
+        renderVerticalLine(context, i - 1, j, l, z, background);
+        renderVerticalLine(context, i + k, j, l, z, background);
+        renderBorder(context, i, j + 1, k, l, z, borderColorStart, borderColorEnd);
+
+        width -= 6;
+        renderHorizontalLine(context, z, x + 3, y + 19, x + 3 + width / 2, y + 20, 0x007F0200, 0xBF7F0200);
+        renderHorizontalLine(context, z, x + 3 + width / 2, y + 19, x + 3 + width, y + 20, 0xBF7F0200, 0x007F0200);
+    }
+
+    private static void renderBorder(DrawContext context, int x, int y, int width, int height, int z, int startColor, int endColor) {
+        renderVerticalLine(context, x, y, height - 2, z, startColor, endColor);
+        renderVerticalLine(context, x + width - 1, y, height - 2, z, startColor, endColor);
+        renderHorizontalLine(context, x, y - 1, width, z, startColor);
+        renderHorizontalLine(context, x, y - 1 + height - 1, width, z, endColor);
+    }
+
+    private static void renderVerticalLine(DrawContext context, int x, int y, int height, int z, int color) {
+        context.fill(x, y, x + 1, y + height, z, color);
+    }
+
+    private static void renderVerticalLine(DrawContext context, int x, int y, int height, int z, int startColor, int endColor) {
+        context.fillGradient(x, y, x + 1, y + height, z, startColor, endColor);
+    }
+
+    private static void renderHorizontalLine(DrawContext context, int x, int y, int width, int z, int color) {
+        context.fill(x, y, x + width, y + 1, z, color);
+    }
+
+    private static void renderRectangle(DrawContext context, int x, int y, int width, int height, int z, int color) {
+        context.fill(x, y, x + width, y + height, z, color);
+    }
+
+    public static void renderHorizontalLine(DrawContext context, int zLevel, int left, int top, int right, int bottom, int startColor, int endColor) {
+        float endAlpha = (float) (endColor >> 24 & 255) / 255.0F;
+        float endRed = (float) (endColor >> 16 & 255) / 255.0F;
+        float endGreen = (float) (endColor >> 8 & 255) / 255.0F;
+        float endBlue = (float) (endColor & 255) / 255.0F;
+        float startAlpha = (float) (startColor >> 24 & 255) / 255.0F;
+        float startRed = (float) (startColor >> 16 & 255) / 255.0F;
+        float startGreen = (float) (startColor >> 8 & 255) / 255.0F;
+        float startBlue = (float) (startColor & 255) / 255.0F;
+
+        RenderSystem.enableDepthTest();
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+
+        VertexConsumer vertexConsumer = context.getVertexConsumers().getBuffer(RenderLayer.getGui());
+        Matrix4f matrix4f = context.getMatrices().peek().getPositionMatrix();
+
+        vertexConsumer.vertex(matrix4f, right, top, zLevel).color(endRed, endGreen, endBlue, endAlpha);
+        vertexConsumer.vertex(matrix4f, left, top, zLevel).color(startRed, startGreen, startBlue, startAlpha);
+        vertexConsumer.vertex(matrix4f, left, bottom, zLevel).color(startRed, startGreen, startBlue, startAlpha);
+        vertexConsumer.vertex(matrix4f, right, bottom, zLevel).color(endRed, endGreen, endBlue, endAlpha);
+        context.draw();
+        RenderSystem.disableBlend();
     }
 
 }
