36b6426363222197ef94432819ccc5aac0dd9280
Sygikal <xtin8873@gmail.com>
2025-09-17 20:00:38 -0700

    formulas!!

--- src/main/java/net/skillz/data/populate/Populator.java
+++ src/main/java/net/skillz/data/populate/Populator.java
@@ -5,7 +5,10 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import dev.sygii.ultralib.data.util.OptionalObject;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.skillz.SkillZMain;
 import net.skillz.level.LevelManager;
+import net.skillz.util.FileUtil;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,25 +28,28 @@ public class Populator {
 
     }
 
-    public Map<String, Integer> getSkillMap(JsonArray skillArray, int restriction) {
+    public Map<String, Integer> getSkillMap(JsonArray skillArray, FormulaRunner formulaRunner) {
         Map<String, Integer> populatedRestriction = new HashMap<>();
 
         for (JsonElement elem : skillArray) {
             JsonObject obj = elem.getAsJsonObject();
             String skillKey = obj.get("skill").getAsString();
-            int base = obj.get("base").getAsInt();
-            int multiply = OptionalObject.get(obj, "multiply", 1).getAsInt();
-            int divide = OptionalObject.get(obj, "divide", 1).getAsInt();
+            String formula = obj.get("formula").getAsString();
 
             if (LevelManager.SKILLS.containsKey(skillKey)) {
-                int max = OptionalObject.get(obj, "max", LevelManager.SKILLS.get(skillKey).maxLevel()).getAsInt();
+                formula = formula.replace("SKILL_MAX", String.valueOf(LevelManager.SKILLS.get(skillKey).maxLevel()));
+                formula = formulaRunner.run(formula);
 
-                int requirement = Math.min(base + ((restriction * multiply) / divide), max);
+                int requirement = Math.round((float)FileUtil.evaluateFormula(formula));
                 if (requirement > 0) populatedRestriction.put(skillKey, requirement);
             }
         }
 
         return populatedRestriction;
+    }
+
+    public interface FormulaRunner {
+        String run(String formula);
     }
 
     public Identifier getId() {
