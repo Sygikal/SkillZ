cc440e9dbb027341a895a52aa5042c4da1f1f89c
Sygikal <xtin8873@gmail.com>
2025-09-21 12:37:38 -0700

    clean up

--- src/main/java/net/skillz/data/PopulateLoader.java
+++ src/main/java/net/skillz/data/PopulateLoader.java
@@ -4,25 +4,14 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import dev.sygii.ultralib.data.loader.SimpleDataLoader;
 import dev.sygii.ultralib.data.util.OptionalObject;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolItem;
-import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import net.skillz.SkillZMain;
 import net.skillz.data.populate.Populator;
 import net.skillz.init.ConfigInit;
 import net.skillz.init.LoaderInit;
-import net.skillz.level.LevelManager;
-import net.skillz.level.restriction.PlayerRestriction;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import static net.skillz.SkillZMain.LOGGER;
 
 public class PopulateLoader extends SimpleDataLoader {
 
