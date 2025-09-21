c0b0c161d8409dcac5d14b59009771532ab9625d
Sygikal <xtin8873@gmail.com>
2025-09-21 12:43:37 -0700

    simplify

--- src/main/java/net/skillz/data/populate/impl/BlockPopulator.java
+++ src/main/java/net/skillz/data/populate/impl/BlockPopulator.java
@@ -4,18 +4,22 @@ import com.google.gson.JsonArray;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.skillz.SkillZMain;
 import net.skillz.data.populate.Populator;
+import net.skillz.init.ConfigInit;
 import net.skillz.init.LoaderInit;
 import net.skillz.level.LevelManager;
 import net.skillz.level.restriction.PlayerRestriction;
 import org.apache.commons.compress.utils.Lists;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,10 +27,12 @@ import java.util.Map;
 
 
 public class BlockPopulator extends Populator {
+    private final List<String> hiddenItems = Lists.newArrayList();
+
     private final List<String> tagWhitelist = Lists.newArrayList();
     private final List<String> tagBlacklist = Lists.newArrayList();
-    private final int minCutoff;
-    private final int maxCutoff;
+    private final float minCutoff;
+    private final float maxCutoff;
     private final List<BlockFilter> filters = Lists.newArrayList();
     private final Map<Block, List<Pair<String, Integer>>> list;
     private final Map<Integer, PlayerRestriction> restrictionMap;
@@ -36,7 +42,7 @@ public class BlockPopulator extends Populator {
     private JsonArray skillArray;
 
 
-    public BlockPopulator(Identifier id, Map<Integer, PlayerRestriction> resmap, Map<Block, List<Pair<String, Integer>>> list, JsonArray additional, JsonArray blockFilters, JsonArray tagWhitelist, JsonArray tagBlacklist, Integer minCutoff, Integer maxCutoff) {
+    public BlockPopulator(Identifier id, Map<Integer, PlayerRestriction> resmap, Map<Block, List<Pair<String, Integer>>> list, JsonArray hiddenTags, JsonArray additional, JsonArray blockFilters, JsonArray tagWhitelist, JsonArray tagBlacklist, float minCutoff, float maxCutoff) {
         super(id);
         this.list = list;
         this.restrictionMap = resmap;
@@ -47,6 +53,8 @@ public class BlockPopulator extends Populator {
         blockFilters.forEach((elem) -> {this.filters.add(BlockFilter.valueOf(elem.getAsString().toUpperCase()));});
 
         additional.forEach((elem) -> {this.additional.add(Identifier.tryParse(elem.getAsString()));});
+        hiddenTags.forEach((elem) -> {this.hiddenItems.add(elem.getAsString());});
+
     }
 
     @Override
@@ -67,6 +75,12 @@ public class BlockPopulator extends Populator {
                         }
                     }
                 }
+                for (String s : tagBlacklist) {
+                    TagKey<Block> tag = TagKey.of(RegistryKeys.BLOCK, Identifier.tryParse(s));
+                    if (block.getDefaultState().isIn(tag)) {
+                        skipped = true;
+                    }
+                }
 
                 if (!skipped || (additional.contains(id))) {
                     Map<String, Integer> populatedRestriction = getSkillMap(skillArray, id, formula -> {
@@ -75,40 +89,44 @@ public class BlockPopulator extends Populator {
                                 replace("HARDNESS", String.valueOf(BlockAlgorithm.HARDNESS.runner.run(block)));
                     });
 
-                    for (Map.Entry<String, Integer> entry : populatedRestriction.entrySet()) {
+                    /*for (Map.Entry<String, Integer> entry : populatedRestriction.entrySet()) {
                         list.computeIfAbsent(block, k -> new ArrayList<>()).add(Pair.of(entry.getKey(), entry.getValue()));
-                    }
+                    }*/
 
-                }
-            }
-        }
-
-        for (Map.Entry<Block, List<Pair<String, Integer>>> entry : list.entrySet()) {
-            boolean skipped = false;
-            for (String s : tagBlacklist) {
-                TagKey<Block> tag = TagKey.of(RegistryKeys.BLOCK, Identifier.tryParse(s));
-                if (entry.getKey().getDefaultState().isIn(tag)) {
-                    skipped = true;
-                }
-            }
-            if (!skipped) {
-                Map<String, Integer> populatedRestriction = new HashMap<>();
-
-                for (Pair<String, Integer> pair : entry.getValue()) {
-                    populatedRestriction.put(pair.getLeft(), pair.getRight());
-                }
-
-                if (!populatedRestriction.isEmpty()) {
-                    if (!tagWhitelist.isEmpty()) {
-                        for (String s : tagWhitelist) {
-                            TagKey<Block> tag = TagKey.of(RegistryKeys.BLOCK, Identifier.tryParse(s));
-
-                            if (entry.getKey().getDefaultState().isIn(tag)) {
-                                restrictionMap.put(Registries.BLOCK.getRawId(entry.getKey()), new PlayerRestriction(Registries.BLOCK.getRawId(entry.getKey()), populatedRestriction));
+                    boolean hidden = block.asItem().equals(Items.AIR);
+                    for (String s : hiddenItems) {
+                        if (s.contains("#")) {
+                            s = s.replace("#", "");
+                            TagKey<Item> tag = TagKey.of(RegistryKeys.ITEM, Identifier.tryParse(s));
+                            if (block.asItem().getDefaultStack().isIn(tag)) {
+                                hidden = true;
+                            }
+                        }else {
+                            if (Registries.ITEM.getId(block.asItem()).equals(Identifier.tryParse(s))) {
+                                hidden = true;
                             }
                         }
-                    } else {
-                        restrictionMap.put(Registries.BLOCK.getRawId(entry.getKey()), new PlayerRestriction(Registries.BLOCK.getRawId(entry.getKey()), populatedRestriction));
+                    }
+
+                    if (!populatedRestriction.isEmpty()) {
+                        int rawId = Registries.BLOCK.getRawId(block);
+                        PlayerRestriction restriction = new PlayerRestriction(rawId, populatedRestriction);
+                        restriction.setHidden(hidden);
+                        if (!tagWhitelist.isEmpty()) {
+                            for (String s : tagWhitelist) {
+                                TagKey<Block> tag = TagKey.of(RegistryKeys.BLOCK, Identifier.tryParse(s));
+
+                                if (block.getDefaultState().isIn(tag)) {
+                                    if (restrictionMap.get(rawId) == null || ConfigInit.MAIN.PROGRESSION.populatorOverride) {
+                                        restrictionMap.put(rawId, restriction);
+                                    }
+                                }
+                            }
+                        } else {
+                            if (restrictionMap.get(rawId) == null || ConfigInit.MAIN.PROGRESSION.populatorOverride) {
+                                restrictionMap.put(rawId, restriction);
+                            }
+                        }
                     }
                 }
             }
@@ -156,6 +174,9 @@ public class BlockPopulator extends Populator {
         }),
         NON_INVISIBLE((block) -> {
             return block.getDefaultState().getRenderType() != BlockRenderType.INVISIBLE;
+        }),
+        NON_AIR_ITEM((block) -> {
+            return !block.asItem().equals(Items.AIR);
         });
 
 
