52296dfdbc64c7991f35905aebb3ee3fb4dbd1c8
Sygikal <xtin8873@gmail.com>
2025-09-22 22:39:02 -0700

    Argument type for command

--- src/main/java/net/skillz/SkillZMain.java
+++ src/main/java/net/skillz/SkillZMain.java
@@ -1,16 +1,21 @@
 package net.skillz;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
+import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.ClampedEntityAttribute;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.skillz.access.LevelManagerAccess;
 import net.skillz.init.*;
 import net.skillz.level.LevelManager;
+import net.skillz.mixin.entity.EntityAccessor;
 import net.skillz.network.LevelServerPacket;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.attribute.EntityAttribute;
@@ -18,6 +23,9 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class SkillZMain implements ModInitializer {
     public static final String MOD_ID = "skillz";
@@ -39,6 +47,9 @@ public class SkillZMain implements ModInitializer {
         LevelServerPacket.init();
         TagInit.init();
         ItemInit.init();
+        //register("ground_search", GroundSearchArgumentType.class, GroundSearchArgumentType::groundSearch);
+        ArgumentTypeRegistry.registerArgumentType(identifierOf("operation"), CommandInit.OperationArgument.class,
+                ConstantArgumentSerializer.of(CommandInit.OperationArgument::operation));
     }
 
     public static boolean shouldRestrictItem(PlayerEntity player, Item item) {
@@ -47,6 +58,19 @@ public class SkillZMain implements ModInitializer {
             if (!levelManager.hasRequiredItemLevel(item)) {
                 player.sendMessage(EventInit.sendRestriction(levelManager.getRequiredItemLevel(item), levelManager), true);
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean shouldRestrictEntity(PlayerEntity player, Entity entity) {
+        if (!player.isCreative() && !player.isSpectator()) {
+            if (!entity.hasControllingPassenger() || !((EntityAccessor) entity).callCanAddPassenger(player)) {
+                LevelManager levelManager = ((LevelManagerAccess) player).getLevelManager();
+                if (!levelManager.hasRequiredEntityLevel(entity.getType())) {
+                    player.sendMessage(EventInit.sendRestriction(levelManager.getRequiredEntityLevel(entity.getType()), levelManager), true);
+                    return true;
+                }
             }
         }
         return false;
